0e4ddc9ab63d97d13ca7f48c9ea2ae57cc970773
Felipe Vieira <felip.3lima@hotmail.com>
2022-06-20 22:23:54 -0300

    Add swipeable views and detail screen

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Navigations from './navs';
-import light from '@/themes/light'
 import { ThemeProvider } from '@shopify/restyle'
 import StatusBar from './components/status-bar';
+import { useAtom } from 'jotai';
+import activeThemeAtom from './states/theme';
 
 const App = () => {
+  const [activeTheme] = useAtom(activeThemeAtom)
   return (
     <NavigationContainer>
-      <ThemeProvider theme={light}>
+      <ThemeProvider theme={activeTheme}>
         <StatusBar />
         <Navigations />
       </ThemeProvider>
--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,6 +1,7 @@
 import light, { Theme } from './light'
+import dark from './dark'
 
-export type ThemeNames = 'light'
+export type ThemeNames = 'light' | 'dark'
 export interface ThemeMeta {
   id: ThemeNames
   name: string
@@ -12,6 +13,11 @@ export const themes: readonly ThemeMeta[] = [
     id: 'light',
     name: 'Default Light',
     theme: light
+  },
+  {
+    id: 'dark',
+    name: 'Default Dark',
+    theme: dark
   }
 ]
 
